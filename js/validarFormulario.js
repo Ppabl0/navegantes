24560b3f4201f2521758561174383f0ae46c7def
DiegoTruP <63624137+DiegoTruP@users.noreply.github.com>
2021-09-23 17:07:34 -0500

    Se modifico en validación de num.
    Se modifico la condición de número de teléfono ahora debe ser mayor a 8

--- js/validarFormulario.js
+++ js/validarFormulario.js
@@ -1,42 +1,40 @@
 function validarFormulario(){
     var nombreForm = document.getElementById('nombre').value;
-var nombre = /^[a-zA-Z\s]+$/;
-if(!nombre.test(nombreForm)) {
-alert('Ingresa solo letras en tu nombre');
-return;
-}
-
-//------validar correo
-
-var correoForm = document.getElementById('correo').value;
-var correoReg = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/;
-
-if(!correoReg.test(correoForm)) {
-alert('Ingresa un correo valido');
-return;
-}
-
-//---------------validar numero
-
-var telefonoForm = document.getElementById('telefono').value;
-var telefonoReg = /^[0-9]+$/;
-
-
-if(!telefonoReg.test(telefonoForm)) {
-alert('Ingresa solo números en el telefono');
-return;
-}
-
-if(telefonoForm.lenght != 10){
-    alert('Número de telefono no valido');
+    var nombre = /^[a-zA-Z-\s]+$/;
+    if(!nombre.test(nombreForm)) {
+    alert('Ingresa solo letras en tu nombre');
     return;
-}
-
-return true;
-
-}
-
-
+    }
+    
+    //------validar correo
+    
+    var correoForm = document.getElementById('correo').value;
+    var correoReg = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/;
+    
+    if(!correoReg.test(correoForm)) {
+    alert('Ingresa un correo valido');
+    return;
+    }
+    
+    //---------------validar numero
+    
+    var telefonoForm = document.getElementById('telefono').value;
+    var telefonoReg = /^[0-9]+$/;
+    
+    
+    if(!telefonoReg.test(telefonoForm)) {
+    alert('Ingresa solo números en el telefono');
+    return;
+    }
+    
+    if(telefonoForm.lenght > 8 && telefonoForm.lenght < 12){
+        alert('Número de telefono no valido');
+        return;
+    }
+    
+    return true;
+    
+    }
 
 
 
