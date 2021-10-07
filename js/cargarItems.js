e0555b588bed1099fb930decdaeebb8b613a136e
DiegoTruP <63624137+DiegoTruP@users.noreply.github.com>
2021-10-06 22:29:35 -0500

    Funcionalidad en Categorias
    Se realizo la función de clasificación de categorías.

--- js/cargarItems.js
+++ js/cargarItems.js
@@ -34,47 +34,16 @@ function addItem(item){
 	const itemsContainer = document.getElementById("listaBikinis");
 	itemsContainer.innerHTML += itemHTML;
 
-
 }
 
 
-function GuardarItemsLocalStorage(){
-	class Item {
-		constructor (name,img,price,category){
-						this.name = name;
-						this.img = img;
-						this.price = price;
-            this.category= category;
-		}
-}
 
-let list = [];
-
-list.push(new Item('Red Summer','/imagenes/UNAPIEZA2.jpg','390','Dama una pieza'));
-list.push(new Item('Fresh Style','/imagenes/CURVY1.jpg','200', 'Dama curvy'));
-list.push(new Item('Top Wave','/imagenes/DOSPIEZAS1.jpg','190', 'Dama dos piezas'));
-list.push(new Item('Twisted Summer','/imagenes/UNAPIEZA3.jpg','175','Dama una pieza'));
-list.push(new Item('Water Sun','/imagenes/CURVY2.jpg','350.50', 'Dama curvy'));
-list.push(new Item('Wave light','/imagenes/DOSPIEZAS2.jpg','500', 'Dama dos piezas'));
-list.push(new Item('Sea peace','/imagenes/CURVY3.jpg','392', 'Dama curvy'));
-list.push(new Item('Beauty love','/imagenes/DOSPIEZAS3.jpg','190', 'Dama dos piezas'));
-list.push(new Item('Fire wave','/imagenes/UNAPIEZA4.jpg','165','Dama una pieza'));
-list.push(new Item('Summer sport','/imagenes/HOMBRE1.jpg','150','Caballero'));
-list.push(new Item('Fresh Beach','/imagenes/HOMBRE2.jpg','170','Caballero'));
-list.push(new Item('Blue island','/imagenes/TRIKINI1.jpg','430','Dama trikini'));
-list.push(new Item('Sky mood','/imagenes/TRIKNI2.jpg','140','Dama trikini'));
-
-
-list = JSON.stringify(list);
-
-localStorage.setItem("Productos",list);
-}
-
-
-function CargarItems(){
+function CargarItemsByCategory(category){
 
   const itemsContainer = document.getElementById("listaBikinis");
 	itemsContainer.innerHTML ='';
+
+  console.log("Entro en la funcion CargarItemsByCategory")
 
 	if(localStorage.getItem("Productos") == null){
     localStorage.setItem("Productos",'[lista]')
@@ -82,13 +51,13 @@ function CargarItems(){
 
   let list  = JSON.parse(localStorage.getItem("Productos"));
 	console.log(list.length)
+
+
 	for(let i = 0; i < list.length ; i++){
-		console.log(list[i].name);
-		addItem(list[i]);
+    if(list[i].category == category){
+      console.log(list[i].name);
+      addItem(list[i]);
+    }
 	}
 
 }
-
-
-GuardarItemsLocalStorage();
-CargarItems();
