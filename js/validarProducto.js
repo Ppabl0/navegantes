4660f5df57d53478a4b0e6e50f1b3d892369345e
Cristobal110998 <cristobal.barajas@alumnos.udg.mx>
2021-10-07 19:40:27 -0500

    validaciones_formulario_cris

--- js/validarProducto.js
+++ js/validarProducto.js
@@ -1,21 +1,75 @@
+var banderaNombre = false;
+var banderaCheck = false;
+var banderaPrecio = false;
+var banderaPiezas = false;
 
-var alertList = document.querySelectorAll('.alert')
-var alerts =  [].slice.call(alertList).map(function (element) {
-  return new bootstrap.Alert(element)
-})
+function validacionDeProducto(
+  nombre,
+  chica,
+  mediana,
+  grande,
+  extraGrande,
+  precio,
+  cantidadPiezas
+) {
+  let cumpleNombre = /^[A-Za-z\s]+$/g;
+  let cumplePrecio = /^[0-9.]+$/;
+  let cumplePiezas = /^[0-9]+$/;
 
-var alertPlaceholder = document.getElementById('liveAlertPlaceholder')
-var alertTrigger = document.getElementById('liveAlertBtn')
+  if (cumpleNombre.test(nombre)) {
+    banderaNombre = true;
+  } else {
+    alert("Por favor ingrese un nombre válido");
+  }
 
-function alert(message, type) {
-  var wrapper = document.createElement('div')
-  wrapper.innerHTML = '<div class="alert alert-' + type + ' alert-dismissible" role="alert">' + message + '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>'
+  if (
+    chica == true ||
+    mediana == true ||
+    grande == true ||
+    extraGrande == true
+  ) {
+    banderaCheck = true;
+  } else {
+    alert("Por favor seleccione una talla");
+  }
 
-  alertPlaceholder.append(wrapper)
-}
+  if (cumplePrecio.test(precio)) {
+    banderaPrecio = true;
+  } else {
+    alert("Por favor ingrese un precio válido");
+  }
 
-if (alertTrigger) {
-  alertTrigger.addEventListener('click', function () {
-    alert('Nice, you triggered this alert message!', 'success')
-  })
+  if (cumplePiezas.test(cantidadPiezas)) {
+    banderaPiezas = true;
+  } else {
+    alert("Por favor ingrese un número de piezas válido");
+  }
+
+  let archivo = document.getElementById("imagenBikini").value;
+  // Obtener extensión del archivo
+  let extension = archivo.substring(archivo.lastIndexOf("."), archivo.length);
+  // Si la extensión obtenida no está incluida en la lista de valores
+  // del atributo "accept", mostrar un error.
+  if (
+    document
+      .getElementById("imagenBikini")
+      .getAttribute("accept")
+      .split(",")
+      .indexOf(extension) < 0
+  ) {
+    alert("Archivo inválido. No se permite la extensión " + extension);
+  }
+
+  if (
+    banderaNombre == false ||
+    banderaCheck == false ||
+    banderaPrecio == false ||
+    banderaPiezas == false
+  ) {
+    return false;
+    //no se puede agregar al local storage
+  } else {
+    return true;
+    //si retorna true se puede agregar al local storage
+  }
 }
