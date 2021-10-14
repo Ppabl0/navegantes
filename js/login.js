e77881d7798ebd0691123f96138dc18be6303baf
Ppabl0 <89792989+Ppabl0@users.noreply.github.com>
2021-10-14 12:11:12 -0500

    validacion y encriptacion
    se añadieron las funciones para poder encriptar la contraseña y validar el correo queel usuario ingresa al iniciar sesion

--- js/login.js
+++ js/login.js
@@ -21,7 +21,7 @@ function anadirRegistro() {
     <div><label>Tu Email:</label><input id="correo" type='email' value='' placeholder="Ejemplo: alberto54@gmail.com" required>
     </div>
     <div><label>Telefono:</label><input id="telefono" type='text' value='' minlength="10" maxlength="10" placeholder="3331310863" required></div>
-    <div><label>Contraseña (8 caracteres minimo):</label><input id="contra" type='password'  value='' minlength="8" placeholder="" required></div>
+    <div><label>Contraseña (8 caracteres minimo):</label><input id="contra" type='password'  value='' minlength="8" placeholder="Al menos una mayúscula, un dígito y un símbolo." required></div>
     <div><label>Confirmar contraseña:</label><input id="contra2" type='password'  value='' placeholder="" required></div>
     <div><input class="btn" style="background-color: #f700cd; color:white;" type="button" onclick="validarFormulario()" value='Registrarme'></div>
     </form>
@@ -69,18 +69,8 @@ function validarFormulario(){
   let contrasena = document.getElementById('contra').value
   let contrasena2 = document.getElementById('contra2').value
 
-  if(contrasena.length < 8){
-    alert('Su contraseña debe tener al menos 8 caracteres');
-    return;
-  }
-
-  if(!/[0-9]+/.test(contrasena)){
-    alert('Su contraseña debe tener almenos 1 digito o numero');
-    return;
-  }
-
-  if(!/\W+/.test(contrasena)){
-    alert('Su contraseña debe tener un caracter que NO sea una letra o numero');
+  if(!/^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,16}$/.test(contrasena)){
+    alert('La contraseña debe contener entre 8 y 16 caracteres, un dígito, una mayúscula y un símbolo.')
     return;
   }
 
@@ -89,14 +79,16 @@ function validarFormulario(){
     return;
   }
 
-  let usuarioNuevo = new Usuario(nombreForm, correoForm, telefonoForm, contrasena);
+  //encriptar contraseña y guardar la contraseña encriptada en los datos del usuario
+  let contraEncriptada = crypt.encrypt(contrasena);
+
+  let usuarioNuevo = new Usuario(nombreForm, correoForm, telefonoForm, contraEncriptada);
   agregarUsuario(usuarioNuevo)
 
   return true;
-  
-  
 }
 
+//añadir el usuario y sus datos al local storage
 function agregarUsuario(usuario){
   
   let nuevoUsuario = JSON.stringify(usuario)
@@ -110,18 +102,56 @@ function agregarUsuario(usuario){
   }
 }
 
-//BOOTSTRAP no me deja tomar lo que ingresa el usuario cuando se aprieta el boton de login
-// function validarInicio(){
-//   let correo = document.getElementById('email').value
-//   let contra = document.getElementById('password').value
-//   let registro = localStorage.getItem(correo)
 
-//   if(registro == null){
-//     alert('no se encontro ningun usuario con el correo: '+ correo)
-//   }else {
-//     let usuario = JSON.parse(registro)
-//     console.log(usuario.contrasena == contra)
-//   }
-// }
+function validarInicio(){
+  //------validar correo
+  let correo = document.getElementById('email').value
+  var correoReg = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/;
+  
+  if(!correoReg.test(correo)) {
+    alert('Ingresa un correo valido');
+    return;
+  }
+
+  //validar que exista el usuario y su contraseña
+  let contra = document.getElementById('password').value
+  let registro = localStorage.getItem(correo)
+
+  if(registro == null){
+    alert('no se encontro ningun usuario con el correo: '+ correo)
+  }else {
+    let usuario = JSON.parse(registro)
+    //desencriptar contraseña del usuario
+    let desencriptada = crypt.decrypt(usuario.contrasena)
+    if(desencriptada == contra){
+      alert('Inicio de sesion existoso')
+      document.getElementById('inicio').reset()
+    }else {
+      alert('La contraseña es incorrecta')
+    }
+    
+  }
+}
+
+//declaracion de las funciones pa encriptar la contraseña
+// (B) ENCRYPT & DECRYPT FUNCTIONS
+var crypt = {
+  // (B1) THE SECRET KEY
+  secret : "CIPHERKEY",
+
+  // (B2) ENCRYPT
+  encrypt : function (clear) {
+    var cipher = CryptoJS.AES.encrypt(clear, crypt.secret);
+    cipher = cipher.toString();
+    return cipher;
+  },
+
+  // (B3) DECRYPT
+  decrypt : function (cipher) {
+    var decipher = CryptoJS.AES.decrypt(cipher, crypt.secret);
+    decipher = decipher.toString(CryptoJS.enc.Utf8);
+    return decipher;
+  }
+}
 
 
